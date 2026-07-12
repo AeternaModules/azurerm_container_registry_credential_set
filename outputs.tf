dff5b5e64301d4f392edbc9a45ddfb8b674d3b17
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:31 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "container_registry_credential_sets" {
-  description = "All container_registry_credential_set resources"
-  value       = azurerm_container_registry_credential_set.container_registry_credential_sets
-}
 output "container_registry_credential_sets_authentication_credentials" {
-  description = "List of authentication_credentials values across all container_registry_credential_sets"
-  value       = [for k, v in azurerm_container_registry_credential_set.container_registry_credential_sets : v.authentication_credentials]
+  description = "Map of authentication_credentials values across all container_registry_credential_sets, keyed the same as var.container_registry_credential_sets"
+  value       = { for k, v in azurerm_container_registry_credential_set.container_registry_credential_sets : k => v.authentication_credentials }
 }
 output "container_registry_credential_sets_container_registry_id" {
-  description = "List of container_registry_id values across all container_registry_credential_sets"
-  value       = [for k, v in azurerm_container_registry_credential_set.container_registry_credential_sets : v.container_registry_id]
+  description = "Map of container_registry_id values across all container_registry_credential_sets, keyed the same as var.container_registry_credential_sets"
+  value       = { for k, v in azurerm_container_registry_credential_set.container_registry_credential_sets : k => v.container_registry_id }
 }
 output "container_registry_credential_sets_identity" {
-  description = "List of identity values across all container_registry_credential_sets"
-  value       = [for k, v in azurerm_container_registry_credential_set.container_registry_credential_sets : v.identity]
+  description = "Map of identity values across all container_registry_credential_sets, keyed the same as var.container_registry_credential_sets"
+  value       = { for k, v in azurerm_container_registry_credential_set.container_registry_credential_sets : k => v.identity }
 }
 output "container_registry_credential_sets_login_server" {
-  description = "List of login_server values across all container_registry_credential_sets"
-  value       = [for k, v in azurerm_container_registry_credential_set.container_registry_credential_sets : v.login_server]
+  description = "Map of login_server values across all container_registry_credential_sets, keyed the same as var.container_registry_credential_sets"
+  value       = { for k, v in azurerm_container_registry_credential_set.container_registry_credential_sets : k => v.login_server }
 }
 output "container_registry_credential_sets_name" {
-  description = "List of name values across all container_registry_credential_sets"
-  value       = [for k, v in azurerm_container_registry_credential_set.container_registry_credential_sets : v.name]
+  description = "Map of name values across all container_registry_credential_sets, keyed the same as var.container_registry_credential_sets"
+  value       = { for k, v in azurerm_container_registry_credential_set.container_registry_credential_sets : k => v.name }
 }
 
