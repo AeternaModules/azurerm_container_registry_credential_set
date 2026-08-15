f1eeddbec767bb1c866a101d213fc1cabb502368
Daniel Allen <dan@aeternagroup.net>
2026-08-15 01:02:36 +0100

    Update to schema version v4.81.0

--- outputs.tf
+++ outputs.tf
@@ -4,7 +4,7 @@ output "container_registry_credential_sets_id" {
 }
 output "container_registry_credential_sets_authentication_credentials" {
   description = "Map of authentication_credentials values across all container_registry_credential_sets, keyed the same as var.container_registry_credential_sets"
-  value       = { for k, v in azurerm_container_registry_credential_set.container_registry_credential_sets : k => v.authentication_credentials if v.authentication_credentials != null && length(v.authentication_credentials) > 0 }
+  value       = { for k, v in azurerm_container_registry_credential_set.container_registry_credential_sets : k => one(v.authentication_credentials) if v.authentication_credentials != null && length(v.authentication_credentials) > 0 }
 }
 output "container_registry_credential_sets_container_registry_id" {
   description = "Map of container_registry_id values across all container_registry_credential_sets, keyed the same as var.container_registry_credential_sets"
@@ -12,7 +12,7 @@ output "container_registry_credential_sets_container_registry_id" {
 }
 output "container_registry_credential_sets_identity" {
   description = "Map of identity values across all container_registry_credential_sets, keyed the same as var.container_registry_credential_sets"
-  value       = { for k, v in azurerm_container_registry_credential_set.container_registry_credential_sets : k => v.identity if v.identity != null && length(v.identity) > 0 }
+  value       = { for k, v in azurerm_container_registry_credential_set.container_registry_credential_sets : k => one(v.identity) if v.identity != null && length(v.identity) > 0 }
 }
 output "container_registry_credential_sets_login_server" {
   description = "Map of login_server values across all container_registry_credential_sets, keyed the same as var.container_registry_credential_sets"
